ba77633531ee136bb9f8a0e03388c2c0b57cd405
Lakdar Arfento <arfento@gmail.com>
2023-07-17 13:11:05 +0700

    Revert "Dev"

--- lib/core/constants.dart
+++
@@ -1,3 +0,0 @@
-class Constants {
-  static const apiKey = "d73f274b69324246816486dc4527138c";
-}
--- lib/domain/repository/news_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:flutter_news/core/failures.dart';
-import 'package:flutter_news/core/news_params.dart';
-import 'package:flutter_news/domain/entities/news.dart';
-
-abstract class NewsRepository {
-  Future<Either<Failure, List<News>>> getNews({required NewsParams parameters});
-}
